5cde97427c9822bdf9d9ef382100b87acc2c77f9
emancho <emancho@umd.edu>
2026-03-30 17:33:54 -0400

    removed path to crochet page, updated projectCard, added new project on project page, updated packages

--- src/ProjectList.jsx
+++ src/ProjectList.jsx
@@ -1,5 +1,6 @@
 //=== Assests
 import techBro from './assets/techbro_drac.png';
+import unemployed from './assets/UHN_pilot.png' 
 import girlDdance from './assets/proj1.png';
 import radioShow from './assets/radio_cover.png';
 
@@ -27,19 +28,19 @@ export const listOfProjects = [
         id:2,
         img: `${techBro}`,
         projTitle:'Tech Bro Dracula',
-        projDesc: `This project is a script I'm working on using Dracula's public domain. The premise is that Dracula is struggling to survive in a world filled with modern-day technology and apps, making it challenging to suck blood while staying incognito. To avoid dying from starvation, Dracula reluctantly embraces the very thing he hates—technology—and transforms into a Tech Bro to satisfy his hunger. Check out the rough draft pilot!`,
+        projDesc: `This project is a tv seres I'm working on using Dracula's public domain. The premise is that Dracula is struggling to survive in the modern-day world filled with technology and apps; how does Dracula suck blood while staying incognito and dying from starvation. Dracula embraces the very thing he hates...technology, and transforms into a Tech Bro to satisfy his hunger. Rough draft of the pilot is out now!`,
         projAction:'POPUP',
-        projResponse:`${R2_DOMAIN}tbd_rough_draft.pdf`,
-        buttonCap: 'SCRIPT'
+        projResponse:`${R2_DOMAIN}TBD_pilot.pdf`,
+        buttonCap: 'SCRIPT HERE'
     },
     {
         id:3,
-        img: `${techBro}`,
-        projTitle:'Crochet Corner',
-        projDesc: "I love crocheting and I love showing off some of the peices I made. If you like what you see then I also do COMMISSIONS!!",
-        projAction:'REDIRECT',
-        projResponse:`/crochet`,
-        buttonCap: 'CHECK IT OUT'
+        img: `${unemployed}`,
+        projTitle:'HoboSexual',
+        projDesc: "This project is a short series. Premise: \"Vergil has been with Nyx with two years and lives off her thanks to this \'talents'\. But what happens when Nyx wants Vergil to get a job and Vergil does everything but that. How far does Vergil's talents go to avoid being homeless?\" Rough draft of the pilot is out now",
+        projAction:'POPUP',
+        projResponse:`${R2_DOMAIN}UHN_pilot.pdf`,
+        buttonCap: 'SCRIPT HERE'
     },
     {
         id:4,
@@ -49,7 +50,6 @@ export const listOfProjects = [
         projAction:'OTHER',
         projResponse: `Author notes: One of the inspirations behind making this album is based on a convo I had with a friend about the state of music in the clubs. I wanted to go beyond making a playlist and thought I might as well make my own music that women can dance to. I may have never made music in my life, but I'm confident when it comes to
          this music shit. I don't have a release date, so please be patient :D`,
-        buttonCap: 'MORE'
     },
 
 ];
