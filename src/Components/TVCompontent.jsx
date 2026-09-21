39246a229d16c01a490a73ea975877b3b0645bce
emancho <emancho@umd.edu>
2026-09-21 15:50:20 -0400

    current working version of TV model

--- src/Components/TVCompontent.jsx
+++ src/Components/TVCompontent.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Box, styled } from '@mui/material';
-import SkipNextIcon from '@mui/icons-material/SkipNext';
+//import SkipNextIcon from '@mui/icons-material/SkipNext';
 import ForwardIcon from '@mui/icons-material/Forward';
 
 /**
